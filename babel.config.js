1865956b90aca851671a9150a5e655ecb64e13bd
barisgudul <mehmetbarisgudul@gmail.com>
2025-06-04 16:09:04 +0300

    04/06

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true,
-    }],
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ['module:react-native-dotenv']
+    ],
+  };
 }; 
